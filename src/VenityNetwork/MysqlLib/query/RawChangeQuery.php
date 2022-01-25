0b17f21fc1cea843d8a0026d3df0eba5c1e56013
AkmalFairuz <akmalf475@gmail.com>
2022-01-25 15:27:57 +0700

    removed Types

--- src/VenityNetwork/MysqlLib/query/RawChangeQuery.php
+++ src/VenityNetwork/MysqlLib/query/RawChangeQuery.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace VenityNetwork\MysqlLib\query;
 
 use VenityNetwork\MysqlLib\MysqlConnection;
-use VenityNetwork\MysqlLib\Utils;
 
 class RawChangeQuery extends Query{
 
     public function execute(MysqlConnection $conn, array $params): mixed{
-        return $conn->change($params[0], Utils::getTypesFromArray($params[1]), ...$params[1])->getAffectedRows();
+        return $conn->change($params[0], ...$params[1])->getAffectedRows();
     }
 }
