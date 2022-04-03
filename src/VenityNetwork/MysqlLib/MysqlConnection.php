bfba1fba41b7d488dcaf22a21b8e342b6178979d
AkmalFairuz <akmalf475@gmail.com>
2022-04-04 06:50:18 +0700

    Provide error message

--- src/VenityNetwork/MysqlLib/MysqlConnection.php
+++ src/VenityNetwork/MysqlLib/MysqlConnection.php
@@ -146,12 +146,12 @@ class MysqlConnection{
             if(!$stmt->bind_param($types, ...$args)) {
                 $stmt->close();
                 $ar = Utils::argsToString($args);
-                throw new MysqlException("Prepare Statement bind_param Error: {$this->mysqli->error} [{$this->mysqli->errno}] (query=`{$query}`,types={$types},args={$ar})");
+                throw new MysqlException("Prepare Statement bind_param Error: {$stmt->error} [{$stmt->errno}] (query=`{$query}`,types={$types},args={$ar})");
             }
             if(!$stmt->execute()) {
                 $stmt->close();
                 $ar = Utils::argsToString($args);
-                throw new MysqlException("Prepare Statement execute Error: {$this->mysqli->error} [{$this->mysqli->errno}] (query=`{$query}`,types={$types},args={$ar})");
+                throw new MysqlException("Prepare Statement execute Error: {$stmt->error} [{$stmt->errno}] (query=`{$query}`,types={$types},args={$ar})");
             }
             $result = $stmt->get_result();
             switch($mode) {
